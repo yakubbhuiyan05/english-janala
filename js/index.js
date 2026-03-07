f763ef4ae51d09cd268070f936b2a1f0baa9df26
Yakub Bhuiyan <yakubbhuiyan05@gmail.com>
2026-03-08 00:08:13 +0600

    display lesson complete

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
-const loadLesson=()=>{
-    fetch('https://openapi.programming-hero.com/api/levels/all')
-    .then((res)=>res.json())
-    .then((json)=>console.log(json.data))
-}
-loadLesson()
+const loadLesson = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((json) => displayLesson(json.data));
+};
+const displayLesson = (lessons) => {
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+  for (let lesson of lessons) {
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+        <button id="lesson-btn-${lesson.level_no}" class="btn btn-outline btn-primary lesson-btn"><i class="fa-solid fa-book-open"></i>Lesson- ${lesson.level_no}</button>
+    `;
+    levelContainer.append(btnDiv);
+  }
+};
+
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((json) => console.log(json));
+};
+loadLevelWord()
+loadLesson();
